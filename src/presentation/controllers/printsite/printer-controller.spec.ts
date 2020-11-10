f000ca9265474bd9971d3b0e1d9d47f470fbb43a
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-10 18:35:47 -0300

    test: ensure that the clientDownloader is called with the correct parameters

--- src/presentation/controllers/printsite/printer-controller.spec.ts
+++ src/presentation/controllers/printsite/printer-controller.spec.ts
@@ -1,4 +1,4 @@
-import { Crawler, responseCrawler } from '../../protocols/crawler'
+import { Crawler, responseCrawler, ClientDownloader, responseClientDownloader } from '../../controllers/printsite/printer-controller-protocols'
 import { PrinterController } from './printer-controller'
 
 const makeCrawler = (): Crawler => {
@@ -13,17 +13,32 @@ const makeCrawler = (): Crawler => {
   return new CrawlerStub()
 }
 
+const makeClientDownloader = (): ClientDownloader => {
+  class ClientDownloaderStub implements ClientDownloader {
+    download (data: any): responseClientDownloader {
+      return {
+        message: 'success_message',
+        isError: false
+      }
+    }
+  }
+  return new ClientDownloaderStub()
+}
+
 interface SutTypes {
   sut: PrinterController
   crawler: Crawler
+  clientDownloader: ClientDownloader
 }
 
 const makeSut = (): SutTypes => {
   const crawler = makeCrawler()
-  const sut = new PrinterController(crawler)
+  const clientDownloader = makeClientDownloader()
+  const sut = new PrinterController(crawler, clientDownloader)
   return {
     sut,
-    crawler
+    crawler,
+    clientDownloader
   }
 }
 
@@ -51,6 +66,27 @@ describe('PrinterController', () => {
   test('Make sure to throw error if the crawler throws error', () => {
     const { sut, crawler } = makeSut()
     jest.spyOn(crawler, 'print').mockReturnValueOnce({ message: 'error_message', isError: true })
+    const httpRequest = {
+      body: 'https://any_site.com'
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse).toEqual({ status: 500, body: new Error('There was an internal error, we apologize') })
+  })
+
+  test('Ensure that the clientDownloader is called with the correct parameters', () => {
+    const { sut, clientDownloader } = makeSut()
+    const clientDownloaderSpy = jest.spyOn(clientDownloader, 'download')
+    const httpRequest = {
+      body: 'https://any_site.com'
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse).toEqual({ status: 200 })
+    expect(clientDownloaderSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Make sure to throw error if the clientDownloader throws error', () => {
+    const { sut, clientDownloader } = makeSut()
+    jest.spyOn(clientDownloader, 'download').mockReturnValueOnce({ message: 'error_message', isError: true })
     const httpRequest = {
       body: 'https://any_site.com'
     }
