eae48bd7ddc860e7134c118f82e9305796f46625
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-10 20:01:10 -0300

    refactor: transform PrinterController handle method to a async function

--- src/presentation/controllers/printsite/printer-controller.spec.ts
+++ src/presentation/controllers/printsite/printer-controller.spec.ts
@@ -3,7 +3,7 @@ import { PrinterController } from './printer-controller'
 
 const makeCrawler = (): Crawler => {
   class CrawlerStub implements Crawler {
-    print (url: string): responseCrawler {
+    async print (url: string): Promise<responseCrawler> {
       return {
         message: 'any_message',
         isError: false
@@ -43,54 +43,54 @@ const makeSut = (): SutTypes => {
 }
 
 describe('PrinterController', () => {
-  test('Ensure that 200 returns if a valid request is sent', () => {
+  test('Ensure that 200 returns if a valid request is sasync ent', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: 'https://any_site.com'
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual({ status: 200 })
   })
 
-  test('Ensure that the crawler is called with the correct body', () => {
+  test('Ensure that the crawler is called with the correct body', async () => {
     const { sut, crawler } = makeSut()
     const crawlerSpy = jest.spyOn(crawler, 'print')
     const httpRequest = {
       body: 'https://any_site.com'
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual({ status: 200 })
     expect(crawlerSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
-  test('Make sure to throw error if the crawler throws error', () => {
+  test('Make sure to throw error if the crawler throws error', async () => {
     const { sut, crawler } = makeSut()
-    jest.spyOn(crawler, 'print').mockReturnValueOnce({ message: 'error_message', isError: true })
+    jest.spyOn(crawler, 'print').mockReturnValueOnce(new Promise(resolve => resolve({ message: 'error_message', isError: true })))
     const httpRequest = {
       body: 'https://any_site.com'
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual({ status: 500, body: new Error('There was an internal error, we apologize') })
   })
 
-  test('Ensure that the clientDownloader is called with the correct parameters', () => {
+  test('Ensure that the clientDownloader is called with the correct parameters', async () => {
     const { sut, clientDownloader } = makeSut()
     const clientDownloaderSpy = jest.spyOn(clientDownloader, 'download')
     const httpRequest = {
       body: 'https://any_site.com'
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual({ status: 200 })
     expect(clientDownloaderSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
-  test('Make sure to throw error if the clientDownloader throws error', () => {
+  test('Make sure to throw error if the clientDownloader throws error', async () => {
     const { sut, clientDownloader } = makeSut()
     jest.spyOn(clientDownloader, 'download').mockReturnValueOnce({ message: 'error_message', isError: true })
     const httpRequest = {
       body: 'https://any_site.com'
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual({ status: 500, body: new Error('There was an internal error, we apologize') })
   })
 })
