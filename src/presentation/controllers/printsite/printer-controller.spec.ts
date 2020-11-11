c2028e919ac94c42f3a1b59d73fff7f486d1e575
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-11 12:52:22 -0300

    refactor: remove client downloader protocol from PrinterController

--- src/presentation/controllers/printsite/printer-controller.spec.ts
+++ src/presentation/controllers/printsite/printer-controller.spec.ts
@@ -1,4 +1,4 @@
-import { Crawler, responseCrawler, ClientDownloader, responseClientDownloader } from '../../controllers/printsite/printer-controller-protocols'
+import { Crawler, responseCrawler } from '../../controllers/printsite/printer-controller-protocols'
 import { PrinterController } from './printer-controller'
 
 const makeCrawler = (): Crawler => {
@@ -13,32 +13,17 @@ const makeCrawler = (): Crawler => {
   return new CrawlerStub()
 }
 
-const makeClientDownloader = (): ClientDownloader => {
-  class ClientDownloaderStub implements ClientDownloader {
-    download (data: any): responseClientDownloader {
-      return {
-        message: 'success_message',
-        isError: false
-      }
-    }
-  }
-  return new ClientDownloaderStub()
-}
-
 interface SutTypes {
   sut: PrinterController
   crawler: Crawler
-  clientDownloader: ClientDownloader
 }
 
 const makeSut = (): SutTypes => {
   const crawler = makeCrawler()
-  const clientDownloader = makeClientDownloader()
-  const sut = new PrinterController(crawler, clientDownloader)
+  const sut = new PrinterController(crawler)
   return {
     sut,
-    crawler,
-    clientDownloader
+    crawler
   }
 }
 
@@ -66,27 +51,6 @@ describe('PrinterController', () => {
   test('Make sure to throw error if the crawler throws error', async () => {
     const { sut, crawler } = makeSut()
     jest.spyOn(crawler, 'print').mockReturnValueOnce(new Promise(resolve => resolve({ message: 'error_message', isError: true })))
-    const httpRequest = {
-      body: 'https://any_site.com'
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual({ status: 500, body: new Error('There was an internal error, we apologize') })
-  })
-
-  test('Ensure that the clientDownloader is called with the correct parameters', async () => {
-    const { sut, clientDownloader } = makeSut()
-    const clientDownloaderSpy = jest.spyOn(clientDownloader, 'download')
-    const httpRequest = {
-      body: 'https://any_site.com'
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual({ status: 200 })
-    expect(clientDownloaderSpy).toHaveBeenCalledWith(httpRequest.body)
-  })
-
-  test('Make sure to throw error if the clientDownloader throws error', async () => {
-    const { sut, clientDownloader } = makeSut()
-    jest.spyOn(clientDownloader, 'download').mockReturnValueOnce({ message: 'error_message', isError: true })
     const httpRequest = {
       body: 'https://any_site.com'
     }
--- src/presentation/controllers/printsite/printer-controller.ts
+++ src/presentation/controllers/printsite/printer-controller.ts
@@ -1,22 +1,16 @@
 import { Crawler } from '../../protocols/crawler'
-import { ClientDownloader, httpRequest, httpResponse, internalError } from './printer-controller-protocols'
+import { httpRequest, httpResponse, internalError } from './printer-controller-protocols'
 
 export class PrinterController {
   private readonly crawler: Crawler
-  private readonly clientDownloader: ClientDownloader
 
-  constructor (crawler: Crawler, clientDownloader: ClientDownloader) {
+  constructor (crawler: Crawler) {
     this.crawler = crawler
-    this.clientDownloader = clientDownloader
   }
 
   async handle (request: httpRequest): Promise<httpResponse> {
     const responseCrawler = await this.crawler.print(request.body)
     if (responseCrawler.isError) {
-      return internalError(new Error('There was an internal error, we apologize'))
-    }
-    const responseClientDownloader = this.clientDownloader.download(request.body)
-    if (responseClientDownloader.isError) {
       return internalError(new Error('There was an internal error, we apologize'))
     }
     return {
