4ee8346ee72263210b9ff0e9539acfd26e6b2c77
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-19 11:07:55 -0300

    feat: routes create in index file

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,11 @@
 import express from 'express'
 import config from '../config/vars'
+import router from './routes'
 
 const app = express()
+
+app.use(express.json())
+
+app.use('/', router)
 
 app.listen(config.PORT)
