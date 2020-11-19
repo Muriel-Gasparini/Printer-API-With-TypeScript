060a73929c2d3d6750ba0680024c85e14e22c697
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-19 11:08:30 -0300

    feat: add a configuration puppeteer launch

--- src/utils/printsite-adapters/captureWebsiteAdapter.ts
+++ src/utils/printsite-adapters/captureWebsiteAdapter.ts
@@ -4,12 +4,18 @@ import captureWebsite from 'capture-website'
 export class CaptureWebsiteAdapter implements Crawler {
   async print (url: string): Promise<responseCrawler> {
     try {
-      await captureWebsite.file(url, 'screenshot.png')
+      await captureWebsite.file(url, 'screenshot.png', {
+        launchOptions: {
+          headless: true,
+          args: ['--no-sandbox', '--disable-setuid-sandbox']
+        }
+      })
       return await new Promise(resolve => resolve({
         message: 'success_message',
         isError: false
       }))
     } catch (error) {
+      console.log(error)
       return await new Promise((resolve, reject) => reject({ message: 'error_message', isError: true }))
     }
   }
