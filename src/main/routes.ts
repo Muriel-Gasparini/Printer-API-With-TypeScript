06bdd5ec36fba705c88c41c857af8a475918ee20
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-19 11:06:06 -0300

    feat: root endpoint send info

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import printerController from './controllers/printController'
 
 const router = Router()
 
+router.post('/print', printerController)
+
 router.get('/', (req, res) => {
-  res.send('test')
+  res.send('mande sua url por uma requisição POST para o endpoint /print. E não se esqueça da URL no body em formato JSON, exemplo de um body correto: { "url": "https://google.com" } *OBS precisa do protocolo http especificado na url :)')
 })
 
 export default router
